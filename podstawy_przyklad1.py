3acbf723f2f1181fe84bafaf9e2d850a088c62ed
Andrzej_Winogradow <andrzej.winogradow@gmail.com>
2021-07-28 20:41:50 +0200

    modified podstawy_przyklad1.py

---
+++ podstawy_przyklad1.py
@@ -0,0 +1,16 @@
+###
+
+Przykładowe ćwiczenie z Pythona
+Cel: stworzenie małej bazy danych, która wyświetli w postaci tabeli oraz wykresu dane użytkownika oraz historię pomiarów jego wagi i bmi
+
+###
+
+###
+input data
+input imie nazwisko i tabela z tego
+input kg czy lb
+input wzrost ft czy cm
+bmi
+tabela data wpisu imie nazwisko kg/lb wzost bmi
+wykres jw
+###
