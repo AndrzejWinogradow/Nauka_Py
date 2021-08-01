289cabc2259555db32b7b9a1356ff2624e647dff
Andrzej_Winogradow <andrzej.winogradow@gmail.com>
2021-08-01 16:43:47 +0200

    add readme.md modify podstawy_przyklad1.py

--- podstawy_przyklad1.py
+++ podstawy_przyklad1.py
@@ -1,16 +1,15 @@
 ###
 
-Przykładowe ćwiczenie z Pythona
-Cel: stworzenie małej bazy danych, która wyświetli w postaci tabeli oraz wykresu dane użytkownika oraz historię pomiarów jego wagi i bmi
+#Przykładowe ćwiczenie z Pythona
+#Cel: stworzenie małej bazy danych, która wyświetli w postaci tabeli oraz wykresu dane użytkownika oraz historię pomiarów jego wagi i bmi
 
 ###
 
-###
-input data
-input imie nazwisko i tabela z tego
-input kg czy lb
-input wzrost ft czy cm
-bmi
-tabela data wpisu imie nazwisko kg/lb wzost bmi
-wykres jw
-###
+#input data
+#input imie nazwisko i tabela z tego
+#input kg czy lb
+#input wzrost ft czy cm
+#bmi
+#tabela data wpisu imie nazwisko kg/lb wzost bmi
+#wykres jw
+
